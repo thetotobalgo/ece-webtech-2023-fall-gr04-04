f328157711012e718c4f9aa1dc1c3372d38ca2e3
thetotobalgo <votre@email.com>
2023-09-28 10:40:31 +0200

    feat : creation of the .gitignore

--- lab/lab3/index.js
+++ lab/lab3/index.js
@@ -1,30 +1,6 @@
-const url = require('url')
 const http = require('http')
-const qs = require('querystring')
-const express = require('express');
-const app = express();
-
-const content = '<!DOCTYPE html>' +
-'<html>' +
-'    <head>' +
-'        <meta charset="utf-8" />' +
-'        <title>ECE AST</title>' +
-'    </head>' + 
-'    <body>' +
-'       <p>Hello World!</p>' +
-'       <a>Salut a tous c\'est l\'alien<a>' +
-'    </body>' +
-'</html>'
-
-const serverHandle = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'})
-
-
-  res.write(content)
-
-  res.end()
-}
+const handles = require('./handles')
 
 http
-.createServer(serverHandle)
+.createServer(handles.serverHandle)
 .listen(8080)

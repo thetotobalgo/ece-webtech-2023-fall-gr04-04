d82d01df63d3d06d555a5d738443ae811079d114
thetotobalgo <votre@email.com>
2023-10-27 10:16:26 +0200

    improve: profiles.js

--- app/pages/profiles.js
+++ app/pages/profiles.js
@@ -1,7 +1,20 @@
 import { fetchServerResponse } from "next/dist/client/components/router-reducer/fetch-server-response";
 
 export default function Profile() {
+    return (
+        <div>
 
+            <Header />
+            {profile ? {
+                <div>
+                <p>username : {profile.username}</p>
+                <p>email : {profile.email}</p>
+                </div>
+            } : {
+                <p>Wait...</p>
+            }}
+        </div>
+    );
 }
 
 export async function getServerSideProps() {

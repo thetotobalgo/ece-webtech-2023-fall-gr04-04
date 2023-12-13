6c4c6f24e6870febbaffbf9abeaf8974d0fafc92
thetotobalgo <votre@email.com>
2023-12-13 13:10:08 +0100

    feat: article likes

--- app/pages/articles/[slug].js
+++ app/pages/articles/[slug].js
@@ -1,13 +1,9 @@
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import Layout from '../../components/Layout';
-
 import md5 from 'md5';
-
 import { useContext } from 'react';
-import UserContext from '../../components/UserContext'
-
-
+import UserContext from '../../components/UserContext';
 
 export default function Article() {
   const router = useRouter();
@@ -16,16 +12,15 @@ export default function Article() {
   const [article, setArticle] = useState(null);
   const [comments, setComments] = useState([]);
   const [newComment, setNewComment] = useState('');
+  const [likedUsers, setLikedUsers] = useState([]); // State for liked users
+  const [isLiked, setIsLiked] = useState(false); // State to track if the user has liked the article
 
-  const { user, email, supabase } = useContext(UserContext)
+  const { user, email, supabase } = useContext(UserContext);
 
   useEffect(() => {
-
     if (slug) {
       fetchArticle();
     }
-
-
   }, [slug]);
 
   const fetchArticle = async () => {
@@ -37,6 +32,10 @@ export default function Article() {
           content,
           created_at,
           email
+        ),
+        articlelikes (
+          email,
+          article_id
         )
       `)
       .eq('slug', slug)
@@ -45,16 +44,47 @@ export default function Article() {
     if (error) {
       console.error('Error fetching article:', error);
     } else {
+      console.log("5");
       setArticle(data);
       setComments(data.comments);
+      setLikedUsers(data.articlelikes.map((like) => like.email));
+      setIsLiked(data.articlelikes.some((like) => like.email === email));
+      console.log("6");
+      console.log('Fetched likes:', data.articlelikes);
     }
   };
 
+  const toggleLike = async () => {
+    try {
+      if (isLiked) {
+
+
+        const { data, error } = await supabase.from('articlelikes').delete().eq('email', email).eq('article_id', article.id);
+
+
+
+
+      } else {
+        console.log("2");
+
+        await supabase.from('articlelikes').upsert([{ article_id: article.id, email }]);
+      }
+
+      console.log("3");
+      setIsLiked((prevIsLiked) => !prevIsLiked);
+
+
+      console.log("4");
+      fetchArticle();
+    } catch (error) {
+      console.error('Error toggling like:', error);
+    }
+  };
+
+
+
+
   const postComment = async () => {
-    console.log('User from context:', user);
-    console.log('User email:', email);
-
-
     const { data, error } = await supabase
       .from('comments')
       .insert([{ article_id: article.id, content: newComment, email: email }])
@@ -62,10 +92,8 @@ export default function Article() {
 
     if (error) {
       console.error('Error posting comment:', error);
-      console.log('User from context:', user);
-      console.log('User email:', email);
     } else {
-      setComments(comments => [...comments, ...(Array.isArray(data) ? data : [data])]);
+      setComments((comments) => [...comments, ...(Array.isArray(data) ? data : [data])]);
       setNewComment('');
     }
   };
@@ -89,7 +117,37 @@ export default function Article() {
       <article style={{ overflowY: 'scroll', maxHeight: '75vh' }}>
         <h1 className="text-4xl font-bold my-4">{article.title}</h1>
         <div className="prose lg:prose-xl" dangerouslySetInnerHTML={{ __html: article.content }}></div>
+
         <section className="mt-10">
+          <div>
+            <h2 className="text-2xl font-bold mb-4">Liked by</h2>
+            <div className="flex flex-row flex-wrap"> {/* Use flex-wrap to arrange images horizontally and wrap to next line if needed */}
+              {likedUsers.map((likedEmail, index) => (
+                <div key={index} className="mb-4 mr-4"> {/* Add mr-4 for spacing between images */}
+                  <img
+                    src={getGravatarUrl(likedEmail)}
+                    alt={`User Gravatar for ${likedEmail}`}
+                    className="w-8 h-8 rounded-full"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </section>
+
+        {user && (
+          <section className="mt-4">
+            <button
+              onClick={toggleLike}
+              className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline`}
+            >
+              {isLiked ? 'Unlike' : 'Like'}
+            </button>
+          </section>
+        )}
+        
+
+        <section className="mt-4"> {/* Add spacing */}
           <div className="border-t pt-4 mt-4">
             <h2 className="text-2xl font-bold mb-">Author</h2>
             <img src={getGravatarUrl(article.email)} alt="Author Gravatar" className="w-8 h-8 rounded-full" />
@@ -135,6 +193,8 @@ export default function Article() {
             </form>
           )}
         </section>
+
+        
       </article>
     </Layout>
   );
